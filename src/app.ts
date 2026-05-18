d465572608828eea0ddd468dc4795e8bef696f4f
princeHrajbhar <princerajbharhariram@gmail.com>
2026-05-18 16:31:49 +0530

    project and db setup

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+//backend\src\app.ts
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server running...");
+});
+
+export default app;
---
+++ src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI as string
+    );
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+  } catch (error) {
+
+    console.log("Database connection failed:", error);
+
+    process.exit(1);
+  }
+};
+
+export default connectDB;
